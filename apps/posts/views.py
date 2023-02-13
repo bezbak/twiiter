cb7c3f43a01d5f560fbfc65dc02327d5e83831ef
bezbak <Cemetey.2019@gmail.com>
2023-02-13 19:31:38 +0600

    thursday

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from apps.posts.models import Posts
 # Create your views here.
+def update_post(request, id):
+    if request.method == 'POST':
+        post = Posts.objects.get(id = id)
+        if request.user == post.user:
+            file = request.FILES.get('file')
+            text = request.POST.get('image')
+            if file:    
+                try:
+                    post.image = file
+                    return redirect('index')
+                except:
+                    post.video = file
+                    return redirect('index')
+            else:
+                post.text=text
+                return redirect('index')
+        else:
+            return redirect('update_post', post.id)
+    context = {
+        'post':post
+    }    
+    return render(request, 'update_post.html', context)
