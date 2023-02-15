631b7b17da7f1c017c0d144004a6b8825eb73697
bezbak <Cemetey.2019@gmail.com>
2023-02-15 20:17:26 +0600

    profile_image

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from apps.users.models import User
+from apps.posts.models import Posts, Comment, Like
 from apps.settings.models import Settings
 # Create your views here.
 
@@ -48,6 +49,41 @@ def user_login(request):
 
 def account(request, username):
     user = User.objects.get(username = username)
+    if request.method == 'POST':
+        if 'create_post' in request.POST:
+            post_text = request.POST.get('post_text')
+            post_file = request.FILES.get('post_file')
+            if post_file:
+                try:
+                    post = Posts.objects.create(image = post_file, user = request.user)
+                    post.save()
+                    return redirect('index')
+                except:
+                    post = Posts.objects.create(video = post_file, user = request.user)
+                    post.save()
+                    return redirect('index')
+            else:
+                post = Posts.objects.create(text = post_text, user = request.user)
+                post.save()
+                return redirect('index')
+        if 'like' in request.POST:
+            post = request.POST.get('post')
+            try:
+                like = Like.objects.get(user = request.user, post_id = post)
+                like.delete()
+                return redirect('index')
+            except:
+                like = Like.objects.create(user = request.user, post_id = post)
+                return redirect('index')
+        if 'comment' in request.POST:
+            text = request.POST.get('text')
+            post = request.POST.get('post')
+            try:
+                comment = Comment.objects.create(post_id = post, user = request.user, text = text)
+                comment.save()
+                return redirect('index')
+            except:
+                return redirect('index')
     context = {
         'user':user
     }
